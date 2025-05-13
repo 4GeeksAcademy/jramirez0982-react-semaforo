78d5768527723b0f01503451a498c8ef802c4c96
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-05-13 14:16:14 +0000

    [Creacion de semaforo] Uso de Hook useState

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Button } from "bootstrap";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+	const [color, setColor] = useState(null);
+
+
+
+	return (
+		<div className="container text-center">
+			<h1 className="mt-2 mb-3 p-3">SEMAFORO</h1>
+			<h5>Julian Andres Ramirez</h5>
+			<div className="d-inline-flex flex-column align-items-center bg-dark m-5 p-3 border border-5">
+				<button className={color == "red" ? "btn btn-danger m-1 p-5 rounded-circle btn-lg  btn-light-on" : "btn btn-danger m-1 p-5 rounded-circle btn-lg"} type="button"
+					onClick={() =>
+						setColor("red")}>{color == "red" ? "ON" : ""}</button>
+
+				<button className={color == "yellow" ? "btn btn-warning m-1 p-5 rounded-circle btn-lg  btn-light-on" : "btn btn-warning m-1 p-5 rounded-circle btn-lg"} type="button"
+					onClick={() =>
+						setColor("yellow")}>{color == "yellow" ? "ON" : ""}</button>
+
+				<button className={color == "green" ? "btn btn-success m-1 p-5 rounded-circle btn-lg  btn-light-on" : "btn btn-success m-1 p-5 rounded-circle btn-lg"} type="button"
+					onClick={() =>
+						setColor("green")} >{color == "green" ? "ON" : ""}</button>
+			</div>
+
 		</div>
 	);
 };
